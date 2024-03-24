4701754379dbbcc70f8f201fb7ed18e1f01c71e4
Jun Seong Pyo <standardstar@m365.hanyang.ac.kr>
2024-03-25 01:05:15 +0900

    fix eslint err

--- src/containers/socket/events.tsx
+++
@@ -1,9 +0,0 @@
-export function Events({ events }: { events: any[] }) {
-  return (
-    <ul>
-      {events.map((event, index) => (
-        <li key={index}>{event}</li>
-      ))}
-    </ul>
-  );
-}
